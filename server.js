7610312c844cf3d3e725bbafa65ec81aaf92eb56
DesmondLoo <looatzoo@hotmail.com>
2024-11-11 07:21:47 +0000

    TEST

--- server.js
+++ server.js
@@ -96,6 +96,7 @@ app.post("/delete/:id", async (req, res) => {
     }
     app.get('/demo', function(req, res) {
       res.render('pages/demo');
+    })
   });
 
 // Tells the app which port to run on
